8d2147fd9ff2232102f848992d63c71a65f9d218
aksenovaannaa <anya.axenova2011@gmail.com>
2024-11-20 20:14:33 +0300

    Add files via upload

---
+++ peremen.py
@@ -0,0 +1,5 @@
+number_homework=12
+number_time=1.5
+name_kurs='Python'
+number_hour=number_time/number_homework
+print('Курс:', name_kurs, ',', 'всего задач:', number_homework, ',', 'затрачено часов:', number_time, ',', 'среднее время выполнения', number_hour, 'часа.')
